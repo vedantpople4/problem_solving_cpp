2427ce934fadb149335a2d4947cbdb9c652097e6
Vedant pople <vedantpople4@gmail.com>
2020-03-26 23:08:18 +0530

    Create 1328A.cpp

---
+++ 1328A.cpp
@@ -0,0 +1,36 @@
+You are given two positive integers a and b. In one move you can increase a by 1 (replace a with a+1). Your task is to find the minimum number of moves you need to do in order to make a divisible by b. It is possible, that you have to make 0 moves, as a is already divisible by b. You have to answer t independent test cases.
+
+#Input
+The first line of the input contains one integer t (1≤t≤104) — the number of test cases. Then t test cases follow.
+
+The only line of the test case contains two integers a and b (1≤a,b≤109).
+
+#Output
+For each test case print the answer — the minimum number of moves you need to do in order to make a divisible by b.
+
+#CODE:
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	while(n--)
+	{
+		int a , b;
+		cin>>a>>b;
+		int c = a/b;
+		int d = c+1;
+		int e = a%b;
+		if(e==0)
+		cout<<e<<endl;
+		else
+		{
+		int f = d*b;
+		int g = f-a;
+		cout<<g<<endl;
+		}
+	}
+	return 0;
+}
