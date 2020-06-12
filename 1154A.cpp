f08c797d02180393a635156a0b2abe40e4f8b69e
Vedant pople <vedantpople4@gmail.com>
2020-06-12 21:24:20 +0530

    Add files via upload

---
+++ 1154A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a[4];
+	for(int i=0;i<4;i++)
+	cin>>a[i];
+	sort(a,a+4);
+	int max = a[3];
+	for(int i=0;i<3;i++)
+	cout<<max-a[i]<<" ";
+	return 0;
+}
