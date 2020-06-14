db25c21c81abacf602ea7dba431e8d74f27836bf
Vedant pople <vedantpople4@gmail.com>
2020-06-14 20:09:56 +0530

    Add files via upload

---
+++ 110A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int n,p,q, count=0;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>p>>q;
+		if(q-p>=2)
+		count++;
+		
+	}
+	cout<<count;
+		
+return 0;
+}
