9754c630a70602574cd0c21eea68be2ff9ce4fa3
Vedant pople <vedantpople4@gmail.com>
2020-08-11 16:16:47 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+        
+            public static void main(String[] args){
+                int a = 130;
+                byte b = (byte)a;
+                System.out.println(a);
+                System.out.println(b);
+            }
+        
+}
