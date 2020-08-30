4777c62f6924ba6ebeffa5651691a1c77e15e272
Vedant pople <vedantpople4@gmail.com>
2020-08-30 21:47:00 +0530

    Add files via upload

---
+++ 1341A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define ll long long 
+#define testcase long long t; cin>>t;
+
+int main() {
+	int q;
+	cin >> q;
+	while (q--) {
+		int n, a, b, c, d;
+		cin >> n >> a >> b >> c >> d;
+		int L = n * (a - b), R = n * (a + b);
+		if (R < c - d || c + d < L)
+		    cout << "No\n";
+		else
+		    cout << "Yes\n";
+	}
+}
