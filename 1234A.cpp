abc78059ac279431aa1ec885297a2cfb6c03fcc3
Vedant pople <vedantpople4@gmail.com>
2020-11-21 21:44:54 +0530

    Add files via upload

---
+++ 1234A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define ll long long 
+#define testcase long long t; cin>>t;
+
+int main()
+{
+	testcase;
+	while(t--){
+		ll n;
+		cin>>n;
+		ll a[n];
+		for(ll i=0;i<n;i++) cin>>a[i];
+		
+	}
+}
