dc0bf8dd4f4c78ab33a9b57a26817ac298bb9558
Vedant pople <vedantpople4@gmail.com>
2020-11-23 22:55:35 +0530

    Add files via upload

---
+++ 822A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define db double
+#define pb push_back
+#define mpr make_pair
+#define andl "\n"
+#define f first
+#define s second
+#define mset(x,y) memset(x,y,sizeof(x))
+#define fr(i,n) for(long long int i=0;i<n;i++)
+#define trace(it,x) for(auto it = (x).begin(); it != (x).end(); it++)
+#define mod 1000000007
+#define fastio ios::sync_with_stdio (false); cin.tie (0); cout.tie (0);
+#define runtime cerr<< '\n' << "Time elapsed :" << clock() * 1000.0 / CLOCKS_PER_SEC << " ms\n" ;
+using namespace std;
+ 
+//void solve(){}
+ll fact(ll n)
+{
+	return (n==1 || n==0) ? 1 : n * fact(n-1);
+}
+
+
+
+int main()
+{
+	fastio
+	ll a,b;
+	cin>>a>>b;
+	cout<<fact(min(a,b))<<andl;
+	
+	runtime;
+	
+	
+	
+}
