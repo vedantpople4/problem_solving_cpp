c71b545834f2c18c9763d3d7a19e8515a4c2d5f8
Vedant pople <vedantpople4@gmail.com>
2020-11-25 22:34:07 +0530

    Add files via upload

---
+++ 1303A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define db double
+#define pb push_back
+#define mpr make_pair
+#define andl "\n"
+#define f first
+#define s second
+#define mset(x,y) memset(x,y,sizeof(x))
+#define fr(i,n) for(long long int i=0;i<n;i++)
+#define trace(it,x) for(auto it = (x).begin(); it != (x).end(); it++)
+#define mod 1000000007
+#define fastio ios::sync_with_stdio (false); cin.tie (0); cout.tie (0);
+#define runtime cerr<< '\n' << "Time elapsed :" << clock() * 1000.0 / CLOCKS_PER_SEC << " ms\n" ;
+using namespace std;
+ 
+void solve()
+{
+	string a;
+	cin>>a;
+	ll l=0, r=a.size()-1,ans=0;
+	while(l<=r && a[l]=='0') ++l;
+	while(r>=l && a[r]=='0') --r;
+	for(ll i=l;i<=r;i++) ans+=(a[i]=='0');
+	cout<<ans<<andl;	
+}
+
+
+int main()
+{
+	fastio
+	ll t;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	runtime;
+	
+	
+	
+}
