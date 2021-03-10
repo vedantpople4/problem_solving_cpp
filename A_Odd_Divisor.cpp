5d4e4a543951f9ddfb28efc2dc2f30ba0fa5fa42
Vedant pople <vedantpople4@gmail.com>
2021-03-10 23:58:38 +0530

    update 1475A

--- A_Odd_Divisor.cpp
+++ A_Odd_Divisor.cpp
@@ -1,62 +1,23 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <set>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <limits>
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
+#include <bits/stdc++.h>
+
 using namespace std;
+using ll = long long;
+using ld = long double;
 
-#define fr(i, b, c) for (int i=(b); i < (c); i++) 
-#define fre(a, b, c) for (int(a)=(b); (a) <= (c); ++(a)) 
-#define frn(a, b, c) for (int(a)=(b); (a) >= (c); --(a)) 
-#define frs(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a)) 
-#define frc(a, b, c) for (char(a)=(b); (a) <= (c); ++(a)) 
-typedef long long ll;
-#define testcase long long t; cin>>t;
-
-int main()
-{
-    testcase;
-    while(t--){
-        ll n; 
-        cin>>n;
-        for(ll i=3;i<n;i+=2)
-        {
-            if(n%i==0 && i%2==1)
-            {
-            cout<<"YES"<<endl;
-            break;
-            }
-
-        }
-
-    }
-
-
-
-
-
-
-
-    return 0;
+void solve() {
+  ll n;
+  cin >> n;
+  if (n & (n - 1)) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
 }
 
-
-
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
