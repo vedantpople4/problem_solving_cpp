9cea0d183ed61afd1499969ac5c92bbf25f2dddf
Vedant pople <vedantpople4@gmail.com>
2021-03-10 23:56:33 +0530

    1475A completed

--- A_Boring_Apartments.cpp
+++ A_Boring_Apartments.cpp
@@ -1,39 +1,20 @@
+#include<cstdio>
 #include<iostream>
-#include<stdio.h>
-#define ll long long int
-#define db double
-#define pb push_back
-#define mpr make_pair
-#define andl "\n"
-#define f first
-#define s second
-#define mset(x,y) memset(x,y,sizeof(x))
-#define fr(i,n) for(long long int i=0;i<n;i++)
-#define trace(it,x) for(auto it = (x).begin(); it != (x).end(); it++)
-#define mod 1000000007
-#define fastio ios::sync_with_stdio (false); cin.tie (0); cout.tie (0);
-using namespace std;
- 
+#define ll long long int;
+#define fr(i,n) for(i=0;i<n;i++);
+#define testcase ll t; cin>>t; while(t--)
 
-int main()
-{
-	fastio
-	ll t;
-	cin>>t;
-	while(t--){
-		ll n,i=0;
-        cin>>n;
-        ll ans = ((n%10)*10)-10;
-        while(n!=0){
-            n/=10;
-            i++;
-        }
-        if(i==1) ans+=1;
-        else if(i==2) ans+=3;
-        else if(i==3) ans+=6;
-        else if(i==4) ans+=10;
+int main(){
+
+    long t; 
+    cin>>t;
+    while(t--){
+        long x; 
+        cin>>x;
+        
+        long ans = 10 * ((x % 10) - 1) + (x / 1 > 0) + 2*(x / 10 > 0) + 3*(x / 100 > 0) + 4*(x / 1000 > 0);
         cout<<ans<<endl;
+    }
 
-	}
-	
+    return 0;
 }
