d5b6b813dba5addbf0ddbaf8aed99538e8b8f389
Vedant pople <vedantpople4@gmail.com>
2021-03-12 23:20:13 +0530

    1472A done

--- A_Cards_for_Friends.cpp
+++ A_Cards_for_Friends.cpp
@@ -1,59 +1,26 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <set>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <limits>
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
+#include<iostream>
+#include<cstdio>
 using namespace std;
-
-#define fr(a, b, c) for (int(a)=(b); (a) < (c); ++(a)) 
-#define fre(a, b, c) for (int(a)=(b); (a) <= (c); ++(a)) 
-#define frn(a, b, c) for (int(a)=(b); (a) >= (c); --(a)) 
-#define frs(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a)) 
-#define frc(a, b, c) for (char(a)=(b); (a) <= (c); ++(a)) 
-typedef long long ll;
-#define testcase long long t; cin>>t; while(t--);
+using ll = long long;
 
 int main()
 {
-testcase;
-while(t--){
-    ll w,h,n;
-    cin>>w>>h>>n;
-    if(n==1) cout<<"YES"<<endl;
-    else{
-        ll count1 = 0, count2=0;
-        while(w!=1){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll w,h,n;
+        cin>>w>>h>>n;
+        ll ans = 1;
+        while(w%2==0){
             w/=2;
-            count1++;
+            ans*=2;
         }
-        while(h!=1){
+        while(h%2==0){
             h/=2;
-            count2++;
+            ans*=2;
         }
-        if(count1+count2>=n) cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
+        cout<<(ans>=n  ? "YES" : "NO")<<endl;
+
     }
+    return 0;
 }
-return 0;
-}
-
-
-
