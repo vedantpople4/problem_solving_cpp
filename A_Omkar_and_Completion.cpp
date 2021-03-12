ab7edd90e2aea24b7e8cd4b58819dd94650807a3
Vedant pople <vedantpople4@gmail.com>
2021-03-13 00:00:27 +0530

    1372A

---
+++ A_Omkar_and_Completion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        while(n--) cout<<4<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
