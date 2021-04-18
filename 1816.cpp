256ac93f6cfa224b48a9520b78c1e10b1b061d77
Vedant pople <vedantpople4@gmail.com>
2021-04-18 21:01:07 +0530

    Create 1816.cpp

---
+++ 1816.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) 
+    {
+        int cnt=0;
+        string str="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]==' ')
+                cnt++;
+            if(cnt==k)
+                break;
+            str.push_back(s[i]);
+        }
+        return str;
+    }
+};
